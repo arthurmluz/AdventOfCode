83c0c8e163094cd8a77be50995458771122d3fa4
Arthur Luz <arthur.musskopf@nubo.ai>
2021-12-10 14:34:46 -0300

    Dia 9 com desenhos

--- 21/day9.py
+++ 21/day9.py
@@ -1,3 +1,8 @@
+from termcolor import colored
+from random import randint
+import time
+
+
 f = open("day9.txt", "r")
 
 matriz = []
@@ -33,7 +38,33 @@ for j in range(len(matriz)):
 
 print(part1)
 
-def bfs(low_point, basins, basins_toPrint, count):
+def imprime(basins_toPrint):
+    print('\033[2J\033[H', end="")
+    for j in range(int(len(matriz)/2)):
+        for i in range(int((len(line)-1))):
+            if (j,i) in basins_toPrint:
+                random = basins_toPrint[(j,i)]
+                if random == 0:
+                    print(colored(str('▓'), 'red'), end="")
+                elif random == 1:
+                    print(colored(str('▓'), 'cyan'), end="")
+                elif random == 2:
+                    print(colored(str('▓'), 'yellow'), end="")
+                elif random == 3:
+                    print(colored(str('▓'), 'blue'), end="")
+                elif random == 4:
+                    print(colored(str('▓'), 'green'), end="")
+                else:
+                    print(colored(str('▓'), 'magenta'), end="")
+            else:
+                print(colored(str('▓'), 'white'), end="")
+        print("")
+    time.sleep(0.09)
+
+
+                
+
+def bfs(low_point, basins, basins_toPrint, random):
     global part2
 
     lista = [low_point]
@@ -57,14 +88,17 @@ def bfs(low_point, basins, basins_toPrint, count):
             lista.append((j,i-1))
             basin.append((j,i-1))
 
-        basins_toPrint[(j,i)] = count
+        basins_toPrint[(j, i)] = random
+        if j <= int(len(matriz)/2) and i <= (len(line)-1):
+            imprime(basins_toPrint)
 
     basins.append(basin)
 
 basins_toPrint = {}
 basins = []
 for idx, i in enumerate(low_points):
-    bfs(i, basins, basins_toPrint, idx)
+        bfs(i, basins, basins_toPrint, randint(0,6))
+
 
 a = sorted(list(map(len, basins)), reverse=True)
 part2 = a[0] * a[1] * a[2] 
