76b02f926d4e4d4f8e35853212c57e47da2d08ec
Arthur Luz <arthur.musskopf@nubo.ai>
2021-12-15 19:33:36 -0300

    Dia 15 2021 atualizado com heaps

--- 21/day15.py
+++ 21/day15.py
@@ -7,15 +7,6 @@ f = open(sys.argv[1], 'r')
 matrix = {(numLines, numColumns): value for numLines, line in enumerate(f) for numColumns, value in enumerate(list(map(int, line[:-1])))}
 last = list(matrix)[-1]
 
-def imprime():
-    for i in range(last2[0]+1):
-        for j in range(last2[1]+1):
-            if i < last[0]+1 and j < last[1]+1:
-                print(colored(str(matrix2[(i,j)]), 'red'), end = "")
-            else:
-                print(matrix2[(i,j)], end="")
-        print("")
-
 matrix2 = copy.deepcopy(matrix)
 for lines in range(0, (last[0]+1)):
     for columns in range(last[1]+1, (last[1]+1)*5):
@@ -30,27 +21,15 @@ for lines in range(last[0]+1, (last[0]+1)*5):
 
 last2 = list(matrix2)[-1]
 
-def retiraMin(lista, dist):
-    menor = sys.maxsize
-    menorNodo = None
-    for i in lista:
-        if dist[i] <= menor:
-            menorNodo = i
-            menor = dist[i]
-    return menorNodo
-
 def dijkstra(grafo):
     dist,prev = {}, {}
     lista = []
     for v in grafo:
         dist[v] = sys.maxsize
-#        lista.append(v)
         heappush(lista, (dist[v], v))
 
     dist[(0,0)] = 0
     while(len(lista)):
-#        u = retiraMin(lista, dist)
-#        lista.remove(u)
         u = heappop(lista)[1]
         for i in range(-1, 2):
             nodo = (u[0]+i, u[1]) 
