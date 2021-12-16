64db8f8b872087190a5e3760c30b21b02a86bab1
Arthur Luz <arthur@nubo.ai>
2021-12-15 22:01:01 -0300

    Dia 15 2021 menos linhas

--- 21/day15.py
+++ 21/day15.py
@@ -1,28 +1,22 @@
 import sys, copy
 from heapq import heappop, heappush
-from termcolor import colored
 
 f = open(sys.argv[1], 'r')
-
 matrix = {(numLines, numColumns): value for numLines, line in enumerate(f) for numColumns, value in enumerate(list(map(int, line[:-1])))}
 last = list(matrix)[-1]
 
-matrix2 = copy.deepcopy(matrix)
-for lines in range(0, (last[0]+1)):
-    for columns in range(last[1]+1, (last[1]+1)*5):
-        value = matrix2[(lines,columns-(last[1]+1))]+1
-        matrix2[(lines,columns)] = value if value <= 9 else 1 
+matrix2 = copy.deepcopy(matrix) 
+def generateMatrix(minLine, minColumn, maxLine, maxColumn):
+    for lines in range(minLine, maxLine):
+        for columns in range(minColumn, maxColumn):
+            value = matrix2[(lines-minLine, columns-minColumn)]+1
+            matrix2[(lines,columns)] = value if value <= 9 else 1 
 
-for lines in range(last[0]+1, (last[0]+1)*5):
-    for columns in range(0, (last[1]+1)*5):
-        value = matrix2[(lines-(last[0]+1),columns)]+1
-        matrix2[(lines,columns)] = value if value <= 9 else 1 
-
-
-last2 = list(matrix2)[-1]
+generateMatrix(0, last[1]+1, last[1]+1, (last[1]+1)*5)
+generateMatrix(last[0]+1, 0, (last[0]+1)*5, (last[1]+1)*5)
 
 def dijkstra(grafo):
-    dist,prev = {}, {}
+    dist = {}
     lista = []
     for v in grafo:
         dist[v] = sys.maxsize
@@ -32,22 +26,20 @@ def dijkstra(grafo):
     while(len(lista)):
         u = heappop(lista)[1]
         for i in range(-1, 2):
-            nodo = (u[0]+i, u[1]) 
-            if nodo in grafo:
-                aux = dist[u] + grafo[nodo]
-                if aux < dist[nodo]:
-                    dist[nodo] = aux
-
-        for j in range(-1, 2):
-            nodo = (u[0], u[1]+j) 
-            if nodo in grafo:
-                aux = dist[u] + grafo[nodo]
-                if aux < dist[nodo]:
-                    dist[nodo] = aux
+            for j in range(-1, 2):
+                if j != 0 and i != 0:
+                    continue
+                nodo = (u[0]+i, u[1]+j) 
+                if nodo in grafo:
+                    aux = dist[u] + grafo[nodo]
+                    if aux < dist[nodo]:
+                        dist[nodo] = aux
 
     return dist
 
 dist = dijkstra(matrix)
 print('part1: ', dist[last])
+
+last = list(matrix2)[-1]
 dist = dijkstra(matrix2)
-print('part2: ', dist[last2])
+print('part2: ', dist[last])
